324960b7d52e85e8726adaedf589495e5215edfd
Robert Miller <robertjessemiller@gmail.com>
2021-03-25 12:57:48 -0400

    Updated bootstrap to run app file so tests can be run without activating plugin

--- bootstrap.php
+++ bootstrap.php
@@ -3,3 +3,4 @@
 require_once __DIR__ . '/autoload.php';
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/../../../wp-load.php';
+require_once __DIR__ . '/site-security-headers.php';
