226a8e658e97a2f9958eed2e0db66470c2839230
Robert Miller <robertjessemiller@gmail.com>
2021-03-25 13:50:13 -0400

    Updated the view details links

--- Views/section/piece/details.php
+++ Views/section/piece/details.php
@@ -1,1 +1,1 @@
-<a class="site-security-headers-strong" href="<?=$header['information']['link']?>" rel="noopener" target="_blank">View details</a>
+<a class="site-security-headers-strong" href="<?=$header['information_link']?>" rel="noopener" target="_blank">View details</a>
